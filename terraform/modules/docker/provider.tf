48bc92cc3395e23106c7e2d734009cc5f0857053
Ayodele Ajayi <ayo@intellidevops.me>
2023-10-27 00:20:40 +0100

    Change provider

--- terraform/modules/docker/provider.tf
+++ terraform/modules/docker/provider.tf
@@ -1,11 +1,20 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      version = "~> 2.0"
     }
   }
 }
+
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
